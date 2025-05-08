2efd4a2fd764405d9c348b1882539cad2bd273bd
fakhar-nadeem <fakharnadeem77@gmail.com>
2025-05-08 23:55:08 +0100

    Update Finalapp2.py
    fix

--- Finalapp2.py
+++ Finalapp2.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # Load the merged dataset
+# Loading through google drive because csv was too big to upload on github
 import requests
 
 file_id = "13Fb0d_zRK5oP-3g1MKS1XAxkClsPFsUH"
@@ -174,7 +175,7 @@ elif page == "Model Building":
                       r2_score(y_test, y_pred_svr)]
 
     # Random Forest
-    st.header("6. Random Forest Model")
+    st.header("Random Forest Model")
     rf = RandomForestRegressor(n_estimators=100)
     rf.fit(X_train, y_train)
     y_pred_rf = rf.predict(X_test)
